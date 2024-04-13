7dd463632ab32d54639a08c18cd02ff2dbfc5cd1
Draekk <rodriver1992@gmail.com>
2024-04-13 12:32:42 -0400

    added find client by id endpoint

---
+++ src/main/java/com/draekk/salesmanagement/controllers/AppRestController.java
@@ -0,0 +1,32 @@
+package com.draekk.salesmanagement.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.draekk.salesmanagement.models.dtos.ResponseDto;
+import com.draekk.salesmanagement.services.GeneralServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class AppRestController {
+
+    @Autowired
+    GeneralServiceImpl service;
+
+    //#region Clients
+
+    @GetMapping("/client/find/{id}")
+    public ResponseEntity<ResponseDto<?>> findClientById(@PathVariable Long id) {
+        try {
+            ResponseDto<?> response = service.findCliendById(id);
+            return ResponseEntity.status(response.getStatus()).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+    //#endregion
+}
