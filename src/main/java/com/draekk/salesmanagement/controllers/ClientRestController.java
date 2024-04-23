51c68ea49653f4a8f489b63a583a901607ba9205
Draekk <rodriver1992@gmail.com>
2024-04-23 11:35:13 -0400

    added create client endpoint

--- src/main/java/com/draekk/salesmanagement/controllers/ClientRestController.java
+++ src/main/java/com/draekk/salesmanagement/controllers/ClientRestController.java
@@ -4,11 +4,9 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +42,12 @@ public class ClientRestController {
     @GetMapping("/find/all")
     public ResponseEntity<ResponseDto<?>> findAllClients() {
         ResponseDto<?> response = service.findAllClients();
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto<?>> createClient(@RequestBody Map<String, String> json) {
+        ResponseDto<?> response = service.createClient(json);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
