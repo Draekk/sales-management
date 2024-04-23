3723829113a01ec3203fc6ba5eeb3256e9e4765f
Draekk <rodriver1992@gmail.com>
2024-04-23 11:58:31 -0400

    added update client endpoint

--- src/main/java/com/draekk/salesmanagement/controllers/ClientRestController.java
+++ src/main/java/com/draekk/salesmanagement/controllers/ClientRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,12 @@ public class ClientRestController {
     @PostMapping("/create")
     public ResponseEntity<ResponseDto<?>> createClient(@RequestBody Map<String, String> json) {
         ResponseDto<?> response = service.createClient(json);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto<?>> updateClient(@RequestBody Map<String, Object> json) {
+        ResponseDto<?> response = service.updateClient(json);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
